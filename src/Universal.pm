8d3799adfeb148dc4b4e84f49d639505ebac9ccb
Richard Kelsch <rich@rk-internet.com>
2026-04-07 00:42:40 -0600

    Fixed copyright

--- src/Universal.pm
+++ src/Universal.pm
@@ -2243,6 +2243,11 @@ sub clock {
     return($now);
 }
 
+=head2 COPYRIGHT
+
+Copyright 2023-2026 Richard Kelsch
+All Rights Reserved
+
 =head1 LICENSE
 
 This program is free software; you can redistribute it and/or modify it under the terms of the the Artistic License (2.0). You may obtain a copy of the full license at:
